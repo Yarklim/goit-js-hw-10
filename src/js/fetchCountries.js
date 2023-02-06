013a3feeef3f3430f4b45b220935630317b67462
Yaroslav Klimas <yarklim@gmail.com>
2023-02-06 19:09:16 +0200

    added final solution

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,21 +1,10 @@
-// export function fetchCountries(name) {
-//   const URL = 'https://restcountries.com/v3.1/name/';
-//   const filter = '?fields=name,capital,population,flags,languages';
-//   return fetch(`${URL}${name}${filter}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-
-//     return response.json();
-//   });
-// }
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+const filter = '?fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-  const URL = 'https://restcountries.com/v3.1/name/';
-  const FILTER = '?fields=name,capital,population,flags,languages';
-  return fetch(`${BASE_URL}/${name}?${FILTER}`).then(response => {
-    if (!response.ok) {
-      return new Error(response.status);
+  return fetch(`${BASE_URL}/${name}?${filter}`).then(response => {
+    if (response.status === 404) {
+      return Promise.reject(new Error());
     }
     return response.json();
   });
